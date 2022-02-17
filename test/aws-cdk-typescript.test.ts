8adc8204332b29631033a1ca08b3148472048e2a
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2022-02-17 10:52:41 -0300

    cleanup code

--- test/aws-cdk-typescript.test.ts
+++ test/aws-cdk-typescript.test.ts
@@ -1,16 +1,12 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsCdkTypescript from '../lib/aws-cdk-typescript-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as AwsCdkTypescript from "../lib/aws-cdk-typescript-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-cdk-typescript-stack.ts
-test("SQS Queue Created", () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new AwsCdkTypescript.AwsCdkTypescriptStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+test("AWS CDK Typescript Stack example", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new AwsCdkTypescript.AwsCdkTypescriptStack(app, "MyTestStack");
+  // THEN
+  const template = Template.fromStack(stack);
+  expect(template).toMatchSnapshot();
 });
