a73c84a6a695f2237ba75b08683c12f1d14bd4f8
Vinicius Kiatkoski Neves <viniciuskneves@icloud.com>
2022-03-03 19:55:25 +0100

    [eslint] add template.** to jest expect rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,12 @@ module.exports = {
     "plugin:jest/recommended",
     "plugin:prettier/recommended",
   ],
+  rules: {
+    // Extends https://github.com/jest-community/eslint-plugin-jest/blob/main/src/rules/expect-expect.ts#L55 with template expects
+    // Template expects are defined here: https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.assertions.Template.html
+    "jest/expect-expect": [
+      "warn",
+      { assertFunctionNames: ["expect", "template.**"] },
+    ],
+  },
 };
